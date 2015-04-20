e37e0c23f6ed6dca7e6626b3d3dcd44771e712d4
Arvin Dwarka <benchemployee@PhantomMonkey.local>
2015-04-20 00:57:32 -0700

    update

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'coffeedapp.wsgi.application'
 
 # Database
 
-if ON_HEROKU == '1':
+if ON_HEROKU == True:
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
